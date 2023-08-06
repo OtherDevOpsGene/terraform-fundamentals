dd503caa0b09e57549cbc6a8fafa95679f1aa4b7
Gene Gotimer <otherdevopsgene@portinfo.com>
2023-08-06 12:36:50 -0400

    Initial version

---
+++ 01-hello-world/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.11.0"
+    }
+  }
+
+  required_version = ">= 1.5.4"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "hello_world" {
+  # AMI ID from https://cloud-images.ubuntu.com/locator/ec2/
+  ami           = "ami-02ee5a6c04de8002a"
+  instance_type = "t4g.nano"
+
+  tags = {
+    Name = "hello_world",
+    Owner = "otherdevopsgene@portinfo.com"
+  }
+}
