f4c37fa00d7cc4949e803e6bde99726462823991
Gene Gotimer <otherdevopsgene@portinfo.com>
2023-08-26 11:06:24 -0400

    next lesson

--- 02-multiple-servers/main.tf
+++ 02-multiple-servers/main.tf
@@ -1,22 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.11.0"
-    }
-  }
+resource "aws_instance" "webserver" {
+  count = 2
 
-  required_version = ">= 1.5.6"
-}
-
-provider "aws" {
-  region = "us-east-2"
-}
-
-resource "aws_instance" "hello_world" {
   # AMI ID from https://cloud-images.ubuntu.com/locator/ec2/
   ami           = "ami-02ee5a6c04de8002a"
   instance_type = "t4g.nano"
+  
+  associate_public_ip_address = true
 
   ebs_optimized = true
   monitoring    = true
@@ -26,7 +15,7 @@ resource "aws_instance" "hello_world" {
   }
 
   tags = {
-    Name  = "hello_world"
+    Name  = "webserver-${count.index}"
     Owner = "otherdevopsgene@portinfo.com"
     Class = "Terraform Fundamentals"
   }
