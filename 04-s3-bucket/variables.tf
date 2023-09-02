f22a26bf29146ea7d408d5ca7d78344d2a654cb6
Gene Gotimer <otherdevopsgene@portinfo.com>
2023-09-02 15:24:49 -0400

    initial solution to AWS S3 tfstate

---
+++ 04-s3-bucket/variables.tf
@@ -0,0 +1,10 @@
+variable "initials" {
+  description = "User's initials."
+  type        = string
+}
+
+variable "name" {
+  description = "Component or solution name."
+  type        = string
+  default     = "tfstate"
+}
