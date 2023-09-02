a1bb3121ec324a826ee1be3d20eeb3f214011672
Gene Gotimer <otherdevopsgene@portinfo.com>
2023-09-02 13:28:08 -0400

    providers into separate file

--- 03-simple-module/main.tf
+++ 03-simple-module/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.15.0"
-    }
-  }
-
-  required_version = ">= 1.5.6"
-}
-
-provider "aws" {
-  region = "us-east-2"
-}
-
 module "www" {
   source = "./webserver"
 
