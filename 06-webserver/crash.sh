09cd50dc9a661ba1ee64944e83fe3e710aa04cea
Gene Gotimer <otherdevopsgene@portinfo.com>
2023-09-10 17:58:10 -0400

    Removed instructor scripts

--- 06-webserver/crash.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-aws ec2 describe-instances \
-  --filters "Name=availability-zone,Values=us-east-2a" \
-            "Name=tag:Environment,Values=prod" \
-            "Name=instance-state-name,Values=running" \
-  --query "Reservations[*].Instances[*].[InstanceId]" \
-  --output text | \
-xargs --no-run-if-empty aws ec2 stop-instances --instance-ids
