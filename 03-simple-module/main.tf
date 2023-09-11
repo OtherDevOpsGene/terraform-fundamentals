f422edd6c70a8db8b8fba5d38b6de7b614c649f6
Gene Gotimer <otherdevopsgene@portinfo.com>
2023-09-11 17:40:09 -0400

    Added lesson tag for drift scripts

--- 03-simple-module/main.tf
+++ 03-simple-module/main.tf
@@ -4,9 +4,10 @@ module "www" {
   instance_type = "t4g.nano"
 
   tags = {
-    Name  = var.server_name
-    Owner = var.owner_email
-    Class = var.class_name
+    Name   = var.server_name
+    Owner  = var.owner_email
+    Class  = var.class_name
+    Lesson = "lesson-03"
   }
 }
 
@@ -14,3 +15,4 @@ output "private_ip" {
   description = "Private IP address for the webserver instance."
   value       = module.www.webserver_private_ip
 }
+
